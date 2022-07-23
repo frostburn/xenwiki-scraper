0f8276086f545167a4f6161546034b7bbca0dd20
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-07-23 12:10:26 +0300

    Download more collections parse more files

--- download_files.py
+++ download_files.py
@@ -4,7 +4,10 @@ import requests
 from utils import Soup
 
 Path("downloads/families").mkdir(parents=True, exist_ok=True)
+Path("downloads/clans").mkdir(parents=True, exist_ok=True)
 Path("downloads/subcategories").mkdir(parents=True, exist_ok=True)
+Path("downloads/chromatic_realms").mkdir(parents=True, exist_ok=True)
+Path("downloads/collections").mkdir(parents=True, exist_ok=True)
 
 
 def download_page(url, filename):
@@ -49,3 +52,78 @@ for anchor in container.find_all("a"):
     if not exists(filename):
         print("downloading", url)
         download_page(url, filename)
+
+
+clans_url = f"{base_url}/w/Category:Temperament_clans"
+clans_filename = "downloads/Temperament_clans.html"
+
+if not exists(clans_filename):
+    print("downloading", clans_url)
+    download_page(clans_url, clans_filename)
+
+with open(clans_filename) as fp:
+    soup = Soup(fp)
+
+container = soup.find_all(class_="mw-category")[0]
+
+for anchor in container.find_all("a"):
+    href = anchor.get("href")
+    if href == "/w/Temperament_families_and_clans":
+        continue
+    url = f"{base_url}{href}"
+    name = href.split("/")[-1]
+    filename = f"downloads/clans/{name}.html"
+    if not exists(filename):
+        print("downloading", url)
+        download_page(url, filename)
+
+
+chromatic_realms_url = f"{base_url}/w/Category:Commatic_realms"
+chromatic_realms_filename = "downloads/Chromatic_realms.html"
+
+if not exists(chromatic_realms_filename):
+    print("downloading", chromatic_realms_url)
+    download_page(chromatic_realms_url, chromatic_realms_filename)
+
+with open(chromatic_realms_filename) as fp:
+    soup = Soup(fp)
+
+container = soup.find(class_="mw-content-ltr")
+
+for anchor in container.find_all("a"):
+    href = anchor.get("href")
+    url = f"{base_url}{href}"
+    name = href.split("/")[-1]
+    filename = f"downloads/chromatic_realms/{name}.html"
+    if not exists(filename):
+        print("downloading", url)
+        download_page(url, filename)
+
+
+collections_url = f"{base_url}/w/Category:Temperament_collections"
+collections_filename = "downloads/Temperament_collections.html"
+
+if not exists(collections_filename):
+    print("downloading", collections_url)
+    download_page(collections_url, collections_filename)
+
+with open(collections_filename) as fp:
+    soup = Soup(fp)
+
+container = soup.find_all(class_="mw-category")[1]
+
+for anchor in container.find_all("a"):
+    href = anchor.get("href")
+    url = f"{base_url}{href}"
+    name = href.split("/")[-1]
+    filename = f"downloads/collections/{name}.html"
+    if not exists(filename):
+        print("downloading", url)
+        download_page(url, filename)
+
+chromatic_pairs_url = f"{base_url}/w/Chromatic_pairs"
+chromatic_pairs_filename = "downloads/Chromatic_pairs.html"
+
+if not exists(chromatic_pairs_filename):
+    print("downloading", chromatic_pairs_url)
+    download_page(chromatic_pairs_url, chromatic_pairs_filename)
--- parse_families.py
+++ parse_families.py
@@ -1,8 +1,13 @@
+from fractions import Fraction
 import os
+import re
 
 from utils import Soup
 
-non_names = ["5-limit", "7-limit", "11-limit", "13-limit", "17-limit", "19-limit", "23-limit"]
+non_names = [
+    "5-limit", "7-limit", "11-limit", "13-limit", "17-limit", "19-limit", "23-limit",
+    # "2.5.7.11", "2.5.7.11.13", "2.5.7.11.13.17", "2.3.7.11.13 subgroup", "2.3.5.17.19"
+]
 
 def string_contents(node):
     result = ""
@@ -17,29 +22,40 @@ def string_contents(node):
 def try_parse_subgroup(line):
     try:
         if ":" in line:
-            line = line.split(":")[1].strip()
+            line = line.split(":")[1].strip().split(" ")[0].strip(".")
         if "." in line:
-            return list(map(int, line.split(".")))
+            return list(map(Fraction, line.split(".")))
     except Exception:
         return None
 
+# TODO: Fix Marveltwin
 
-directory = "downloads/families"
-for filename in os.listdir(directory):
-    base, ext = os.path.splitext(filename)
-    if ext != ".html":
-        continue
+directories = [
+    "downloads/chromatic_realms",
+    "downloads/clans",
+    "downloads/collections",
+    "downloads/families"
+]
+filenames = ["downloads/Chromatic_pairs.html"]
+for directory in directories:
+    for filename in os.listdir(directory):
+        base, ext = os.path.splitext(filename)
+        if ext != ".html":
+            continue
+        filenames.append(os.path.join(directory, filename))
+
+for filename in filenames:
     print("...Opening", filename)
-    with open(os.path.join(directory, filename), "r") as fp:
+    with open(filename, "r") as fp:
         soup = Soup(fp)
 
     last_name = None
     for headline in soup.find_all(class_="mw-headline"):
         name = headline.string
-        if name in non_names:
+        if last_name and (name in non_names or "." in name or re.match(r"\d+/\d+", name)):
             name = last_name + " " + name
         else:
-            last_name = name
+            current_name = name
         subgroup = None
         comma_list = None
         mapping = None
@@ -49,10 +65,16 @@ for filename in os.listdir(directory):
         node = headline.parent.next_sibling
         while node and getattr(node, "name", None) not in ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"]:
             contents = string_contents(node).strip()
+            if "NewPP limit report" in contents:
+                node = node.next_sibling
+                continue
+            if "Transclusion expansion time report" in contents:
+                node = node.next_sibling
+                continue
             lower = contents.lower()
             if "subgroup" in lower and not isinstance(subgroup, list):
                 subgroup = try_parse_subgroup(contents) or contents
-            elif "comma list" in lower:
+            elif "comma" in lower:
                 comma_list = contents
             elif "mapping" in lower:
                 mapping = contents
@@ -71,3 +93,4 @@ for filename in os.listdir(directory):
             print(generator)
             print(optimal)
             print(badness)
+            last_name = current_name
