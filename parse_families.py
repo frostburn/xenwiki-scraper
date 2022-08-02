fd304dc1d3f636fe2cb70b52276654b50f8308ca
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-08-02 18:01:56 +0300

    Improve subgroup and comma parsing
    Dump results into a JSON file

--- parse_families.py
+++ parse_families.py
@@ -1,12 +1,13 @@
 from fractions import Fraction
 import os
 import re
+import json
 
 from utils import Soup
 
 non_names = [
     "5-limit", "7-limit", "11-limit", "13-limit", "17-limit", "19-limit", "23-limit",
-    # "2.5.7.11", "2.5.7.11.13", "2.5.7.11.13.17", "2.3.7.11.13 subgroup", "2.3.5.17.19"
+    "Rank-4 temperaments", "Rank five",
 ]
 
 def string_contents(node):
@@ -22,13 +23,33 @@ def string_contents(node):
 def try_parse_subgroup(line):
     try:
         if ":" in line:
-            line = line.split(":")[1].strip().split(" ")[0].strip(".")
+            line = line.split(":")[1].strip()
+        if line.lower() == "full 13-limit":
+            return [Fraction(2), Fraction(3), Fraction(5), Fraction(7), Fraction(11), Fraction(13)]
+        line = line.split(" ")[0].strip(".")
         if "." in line:
             return list(map(Fraction, line.split(".")))
     except Exception:
         return None
 
+def try_parse_commas(line):
+    try:
+        line = line.split(":")[1].strip()
+        return list(map(Fraction, line.replace(" ", ",").replace(",,", ",").split(",")))
+    except Exception:
+        return None
+
+# TODO: Fix
+def parse_mapping(line):
+    print(line)
+    result = []
+    line = line.replace("[", "").replace("⟨", "").replace("<", "").replace(",", "").strip("]")
+    for vec in line.split("]"):
+        result.append(list(map(Fraction, vec.split())))
+    return result
+
 # TODO: Fix Marveltwin
+# TODO: Fix Mercator
 
 directories = [
     "downloads/chromatic_realms",
@@ -44,6 +65,8 @@ for directory in directories:
             continue
         filenames.append(os.path.join(directory, filename))
 
+results = []
+
 for filename in filenames:
     print("...Opening", filename)
     with open(filename, "r") as fp:
@@ -52,12 +75,15 @@ for filename in filenames:
     last_name = None
     for headline in soup.find_all(class_="mw-headline"):
         name = headline.string
+        subtitle = None
         if last_name and (name in non_names or "." in name or re.match(r"\d+/\d+", name)):
-            name = last_name + " " + name
+            title = last_name
+            subtitle = name
         else:
+            title = name
             current_name = name
         subgroup = None
-        comma_list = None
+        commas = None
         mapping = None
         generator = None
         optimal = None
@@ -73,9 +99,9 @@ for filename in filenames:
                 continue
             lower = contents.lower()
             if "subgroup" in lower and not isinstance(subgroup, list):
-                subgroup = try_parse_subgroup(contents) or contents
+                subgroup = try_parse_subgroup(contents)
             elif "comma" in lower:
-                comma_list = contents
+                commas = try_parse_commas(contents)
             elif "mapping" in lower:
                 mapping = contents
             elif "generator" in lower:
@@ -85,12 +111,23 @@ for filename in filenames:
             elif "badness" in lower:
                 badness = contents
             node = node.next_sibling
-        if subgroup and comma_list:
-            print("==", name, "==")
-            print(subgroup)
-            print(comma_list)
-            print(mapping)
-            print(generator)
-            print(optimal)
-            print(badness)
+        if subgroup and commas:
+            results.append({
+                "title": title,
+                "subtitle": subtitle,
+                "subgroup": ".".join(map(str, subgroup)),
+                "commas": ",".join(map(str, commas)),
+                "mapping": mapping,
+                "generator": generator,
+                "optimal": optimal,
+                "badness": badness
+            })
+
             last_name = current_name
+        elif commas:
+            print("failed subgroup", title, subtitle)
+        elif subgroup:
+            print("failed commas", title, subtitle)
+
+with open("json/out.json", "w") as fp:
+    json.dump({"temperaments": results}, fp)
